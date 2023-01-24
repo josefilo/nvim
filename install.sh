33c7af6f21b52f9168cac11c5bbaf8b0faa9e3a0
José Antônio <josefreitas788@gmail.com>
2023-01-24 08:47:28 -0300

    Delete install.sh

--- install.sh
+++
@@ -1,5 +0,0 @@
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-
-git clone https://github.com/github/copilot.vim.git \
-  ~/.config/nvim/pack/github/start/copilot.vim
